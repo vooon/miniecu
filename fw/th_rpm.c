4cdda49409e14096c8ff7dfa3aec78bf33c716f1
Vladimir Ermakov <vooon341@gmail.com>
2014-10-18 15:48:14 +0400

    fw #19: RPM module now works again.
    Returned to average with circular buffer,
    cummulative don't work for unknown reason.

--- fw/th_rpm.c
+++ fw/th_rpm.c
@@ -36,22 +36,26 @@ int32_t gp_rpm_min_idle;
 
 /* -*- private data -*- */
 
-#define UPDATE_TIMEOUT	US2ST(2000000)
+#define PERIODS_MAX		24
+#define UPDATE_TIMEOUT_US	2000000
 
 static float m_curr_rpm;
-static uint32_t m_filtered_period_us;
+static uint32_t m_periods_us[PERIODS_MAX];
 static systime_t m_last_update;
+static uint32_t m_periods_cnt;
+static uint32_t m_periods_idx;
 static THD_WORKING_AREA(wa_rpm, RPM_WASZ);
 
 static void period_handler(ICUDriver *icup);
 static void empty_handler(ICUDriver *icup ATTR_UNUSED);
+static void overflow_handler(ICUDriver *icup ATTR_UNUSED);
 
 static const ICUConfig tim2_cfg = {
 	.mode = ICU_INPUT_ACTIVE_LOW,
 	.frequency = 1000000,		// 1 MHz
-	.width_cb = empty_handler,
+	.width_cb = empty_handler,	// XXX: wait chibios git update with fix
 	.period_cb = period_handler,
-	.overflow_cb = empty_handler,
+	.overflow_cb = overflow_handler,
 	.channel = ICU_CHANNEL_1,
 	.dier = 0
 };
@@ -70,36 +74,56 @@ bool rpm_check_limit(void)
 
 bool rpm_is_engine_running(void)
 {
-	return chVTTimeElapsedSinceX(m_last_update) < UPDATE_TIMEOUT
+	return chVTTimeElapsedSinceX(m_last_update) < US2ST(UPDATE_TIMEOUT_US)
 		&& m_curr_rpm > gp_rpm_min_idle;
 }
 
 /* -*- local -*- */
 
-static void period_handler(ICUDriver *icup)
-{
-	uint32_t new_period_us = icuGetPeriodX(icup);
-
-	m_filtered_period_us = new_period_us;
-
-	// XXX TODO: make filtering
-
-	m_last_update = osalOsGetSystemTimeX();
-}
-
 static void empty_handler(ICUDriver *icup ATTR_UNUSED)
 {
 }
 
+static void period_handler(ICUDriver *icup)
+{
+	// store period in circular buffer
+	m_periods_cnt = (m_periods_cnt < PERIODS_MAX)? m_periods_cnt + 1 : PERIODS_MAX;
+	m_periods_idx = (m_periods_idx >= PERIODS_MAX)? 0 : m_periods_idx;
+	m_periods_us[m_periods_idx++] = icuGetPeriodX(icup);
+
+	m_last_update = osalOsGetSystemTimeX();
+}
+
+static void overflow_handler(ICUDriver *icup ATTR_UNUSED)
+{
+	// clear buffer if overflow
+	m_periods_cnt = 0;
+	m_periods_idx = 0;
+}
+
+static uint32_t get_period_average(void)
+{
+	uint32_t acc = 0;
+
+	for (size_t i = 0; i < m_periods_cnt; i++)
+		acc += m_periods_us[i];
+
+	return acc / m_periods_cnt;
+}
+
 static THD_FUNCTION(th_rpm, arg ATTR_UNUSED)
 {
+	// setup initial values
+	m_periods_idx = 0;
+	m_periods_cnt = 0;
+
 	/* Start input capture
 	 *
 	 * HACK: TIM2 32-bit timer, but driver uses it as 16-bit
 	 * by setting ARR to 0xffff.
 	 */
 	icuStart(&ICUD2, &tim2_cfg);
-	ICUD2.tim->ARR = 2000000;	// 2 sec overflow
+	ICUD2.tim->ARR = UPDATE_TIMEOUT_US;
 	icuStartCapture(&ICUD2);
 	icuEnableNotifications(&ICUD2);
 
@@ -108,17 +132,19 @@ static THD_FUNCTION(th_rpm, arg ATTR_UNUSED)
 		// Update rate: 10 Hz
 		chThdSleepMilliseconds(100);
 
-		uint32_t period = m_filtered_period_us;
+		uint32_t period = get_period_average();
 		systime_t elapsed_time = chVTTimeElapsedSinceX(m_last_update);
 
 		/* Filter out unrealistic period (100 usec ==> RPM 9375.0 with 64 pulses)
 		 * Or if timed out.
 		 */
-		if (period > 100 && elapsed_time < UPDATE_TIMEOUT)
+		if (period > 100 && elapsed_time < US2ST(UPDATE_TIMEOUT_US))
 			m_curr_rpm = 60.0f * 1e6 / (period * gp_pulses_per_revolution);
 		else
 			m_curr_rpm = 0.0f;
 	}
+
+	return MSG_OK;
 }
 
 void rpm_init(void)
